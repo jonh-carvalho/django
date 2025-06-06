f4de7e6afadb718757905227b22105aa9b15caeb
Jonh Carvalho <161327679+jonh-carvalho@users.noreply.github.com>
2025-06-06 13:06:05 +0000

    update createsuperuser

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ python manage.py collectstatic --no-input
 # apply any database migrations that are outstanding
 python manage.py migrate
 
-if [[ $CREATE_SUPERUSER ]];
-then
-  python manage.py createsuperuser --no-input
-fi
+#if [[ $CREATE_SUPERUSER ]];
+#then
+#  python manage.py createsuperuser --no-input
+#fi
