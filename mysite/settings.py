47e0fe0d6cb41895532f3196032f45be6bfd4a70
jonh-carvalho <jonh.carvalho@professores.ibmec.edu.br>
2025-06-20 19:06:33 -0300

    update sqlite

--- mysite/settings.py
+++ mysite/settings.py
@@ -86,12 +86,19 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
+""" DATABASES = {
     'default': dj_database_url.config(
         # TODO update this row to your proper connection string
         default='postgresql://postgres:postgres@localhost:5432/mysite',
         conn_max_age=600
     )
+} """
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
